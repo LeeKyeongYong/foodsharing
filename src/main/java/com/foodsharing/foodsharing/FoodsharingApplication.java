f4061e1c625f1f1ce6dbde552071f31c2877d605
이경용 <sleekydz86@naver.com>
2023-04-08 16:27:47 +0900

    푸드쉐어링 init
    푸드쉐어링 init

---
+++ src/main/java/com/foodsharing/foodsharing/FoodsharingApplication.java
@@ -0,0 +1,13 @@
+package com.foodsharing.foodsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodsharing/foodsharing/FoodsharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodsharing.foodsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
